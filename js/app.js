83a60926e8f6a96e8825d13e09addd6b69ec60fd
Ben Tanen <ben.tanen@gmail.com>
2016-10-14 23:34:07 -0400

    added starter login code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+// initialize spotify stuff
+var spotify = new SpotifyAPI();
+spotify.login.setClientId('7501ca235761432fac0f01e5d91cee1d');
+spotify.login.setRedirect('http://localhost:8000/');
+
+// blank function that does nothing
+function none() { };
+
 $(document).ready(function() {
-    console.log('yes, hello!');
+    console.log('yes, hello! stuff is happening...');
+
+    // log in code
+    spotify.login.pullAccessToken(none, false);
+    $('#login').click(function() {
+        scopes = [
+            'playlist-read-private',
+            'playlist-read-collaborative',
+            'playlist-modify-public',
+            'playlist-modify-private',
+            'user-library-read',
+            'user-library-modify',
+            'user-read-private',
+            'user-read-birthdate',
+            'user-read-email',
+            'user-follow-read',
+            'user-follow-modify',
+            'user-top-read'
+        ];
+        spotify.login.openLogin(scopes);
+    });
 });
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 A tool to algorithmically build yourself a Spotify playlist to party to, workout to, or whatever to.
 
+This project uses [a simplified JavaScript wrapper for the Spotify API](https://github.com/ben-tanen/Spotify.js) written by Ben Tanen.
+
 * * *
 
 #### To Do
